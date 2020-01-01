a4ce4bf939837567fa3f8f71bdc477b0b6598988
Linda <lifaythegoblin@googlemail.com>
2020-01-01 17:31:25 +0100

    addid mini db for team members and created a script to automatically add the team members to the team section on the main page

---
+++ scripts/teamData.js
@@ -0,0 +1,22 @@
+const team = {
+  "Linda": {
+    "interests": "Webentwicklung, Data Science, Scratch",
+    "background": "Studiert Medieninformatik im Master und leitet Informatik-Übungen an der HTW",
+    "tasks": "Projekt Management, Mentoring",
+    "imgUrl": null
+  },
+  "Lotte": {
+    "interests": "Webentwicklung, Organisieren, Menschen",
+    "background": "Studiert Medieninformatik im 4. Semester Bachelor ",
+    "tasks": "Mentoring, ein bisschen Orga",
+    "imgUrl": null
+  },
+  "Sebastian": {
+    "interests": "Von allem ein Bisschen und ein bisschen mehr Autos und Technik. Vor allem aber Spaß am Umgang damit.",
+    "background": "Nfz Mechatroniker (also die ganz schweren Autos), studiert jetzt Fahrzeugtechnik im 1. Semester und probiert gerne alles aus",
+    "tasks": "Mädchen für Alles.",
+    "imgUrl": null
+  },
+}
+
+export {team};
