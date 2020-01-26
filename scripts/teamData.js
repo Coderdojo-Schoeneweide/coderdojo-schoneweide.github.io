a586ed6ccd58ef5f015bdb28fd26fdeb8cd18ba7
Linda <lifaythegoblin@googlemail.com>
2020-01-26 17:38:27 +0100

    added remaining team members

--- scripts/teamData.js
+++ scripts/teamData.js
@@ -1,4 +1,10 @@
 const team = {
+  "Anton": {
+    "interests": "Machine Learning, Mobile Applications, Functional Programming",
+    "background": "Studiert Angewandte Informatik im Master",
+    "tasks": null,
+    "imgUrl": null
+  },
   "Linda": {
     "interests": "Webentwicklung, Data Science, Scratch",
     "background": "Studiert Medieninformatik im Master und leitet Informatik-Übungen an der HTW",
@@ -7,13 +13,37 @@ const team = {
   },
   "Lotte": {
     "interests": "Webentwicklung, Organisieren, Menschen",
-    "background": "Studiert Medieninformatik im 4. Semester Bachelor ",
+    "background": "Studiert Medieninformatik im Bachelor ",
     "tasks": "Mentoring, ein bisschen Orga",
+    "imgUrl": null
+  },
+  "Luis": {
+    "interests": "Game Development, Webentwicklung, Lesen",
+    "background": "Studiert Medieninformatik im Bachelor ",
+    "tasks": "Blog-Maintaining",
+    "imgUrl": null
+  },
+  "Magdalena": {
+    "interests": "Webentwicklung, Webdesign, Graphic Design",
+    "background": "Studiert Medieninformatik im Bachelor ",
+    "tasks": "Logo Design, Social Media und Blog Posts",
+    "imgUrl": null
+  },
+  "Patrick": {
+    "interests": "Ethical Hacking, Software Development, Skateboard, Fußball",
+    "background": "3 Jahre Ausbildung zum Fachinformatiker, studiert jetzt Angewandte Informatik im Bacheor",
+    "tasks": "Mentoring und ein bisschen Social Media",
     "imgUrl": null
   },
   "Sebastian": {
     "interests": "Von allem ein Bisschen und ein bisschen mehr Autos und Technik. Vor allem aber Spaß am Umgang damit.",
-    "background": "Nfz Mechatroniker (also die ganz schweren Autos), studiert jetzt Fahrzeugtechnik im 1. Semester und probiert gerne alles aus",
+    "background": "Nfz Mechatroniker (also die ganz schweren Autos), studiert jetzt Fahrzeugtechnik im Bachelor und probiert gerne alles aus",
+    "tasks": "Mädchen für Alles.",
+    "imgUrl": null
+  },
+  "Yana": {
+    "interests": "Android Development, Lesen",
+    "background": "Vier Semester Business Analyse in Minsk, studiert jetzt Medieninformatik im Bachelor",
     "tasks": "Mädchen für Alles.",
     "imgUrl": null
   },
